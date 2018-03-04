67f76953609bf80a7a290a804b8cd3228c34f39f
Kevin Fu <zkevinfu@gmail.com>
2018-03-04 16:51:34 -0600

    theres gui, its pretty much duct taped on, but everthing works nicely on the surface so its ok

--- e_sim.py
+++ e_sim.py
@@ -13,29 +13,46 @@ def exit_handler(obj):
     print('Finish Exit-')
     obj.update_save()
 
-def cmd_thread(obj,):
+def cmd_thread(obj):
     global running
     prompt = AEIcmd.AEIcmd(obj)
+    # uses sys.in instead of input, this is needed since tkinter dies otherwise
+    prompt.use_rawinput = False
     prompt.run()
     running = False
 
-def decay_thread(obj,):
+def display_thread(obj):
+    global running
+    master = tkinter.Tk()
+    for i, key in enumerate(obj.emotions):
+        tkinter.Label(master, text='%s: '%key).grid(row=i, sticky=tkinter.E)
+    while running:
+        for i, key in enumerate(obj.emotions):
+            tkinter.Label(master, text=obj.emotions[key]).grid(row=i, column = 1, sticky = tkinter.W)
+        try:
+            master.update_idletasks()
+            master.update()
+        except:
+            print ('tk closed')
+        time.sleep(0.1)
+
+def decay_thread(obj):
     global running
     last_time = 0
     while running:
-            if time.time() - last_time > 1:
-                last_time = time.time()
-                obj.decay()
+        if time.time() - last_time > 1:
+            last_time = time.time()
+            obj.decay()
 
 def weather_thread(data_q):
     global running
     last_time = 0
     while running:
-            if time.time() - last_time > 120:
-                last_time = time.time()
-                w_q = locweat.get_stim()
-                while not w_q.empty():
-                    data_q.put(w_q.get())
+        if time.time() - last_time > 120:
+            last_time = time.time()
+            w_q = locweat.get_stim()
+            while not w_q.empty():
+                data_q.put(w_q.get())
 def main():
     if len(sys.argv) == 2:
         name = sys.argv[1]
@@ -53,11 +70,13 @@ def main():
     in_thread = threading.Thread(target = cmd_thread, args = (obj,))
     w_thread = threading.Thread(target = weather_thread, args = (data_q,))
     dec_thread = threading.Thread(target = decay_thread, args = (obj,))
+    dis_thread = threading.Thread(target = display_thread, args = (obj,))
 
     in_thread.start()
     w_thread.start()
     dec_thread.start()
-
+    #dis_thread.start()
+    display_thread(obj)
     while running:
         if not data_q.empty():
             item = data_q.get()
